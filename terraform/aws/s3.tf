575b17afa422c1b208f252594de535b46f17f3de
asiosio <91682683+asiosio@users.noreply.github.com>
2021-11-24 18:16:10 +0100

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "data" {
   # bucket is public
-  # demoo
+  # demooooooo
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
