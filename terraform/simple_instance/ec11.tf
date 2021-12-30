dd259af5fad26f8beda223de868f7272fa71fcdb
asiosio <91682683+asiosio@users.noreply.github.com>
2021-12-30 11:45:51 +0100

    Create ec11.tf

---
+++ terraform/simple_instance/ec11.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "ssh_traffic" {
+  name        = "ssh_traffic"
+  description = "Allow SSH inbound traffic"
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
